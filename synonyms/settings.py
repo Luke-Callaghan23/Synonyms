a51405efc80be3e12f87372b0973b43755090364
Luke Callaghan <luke.callaghan23@gmail.com>
2021-02-18 14:33:29 -0500

    Pushed to heroku

--- synonyms/settings.py
+++ synonyms/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-import dj_database_url
-import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
